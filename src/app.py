4d1a04b4e6f0fb6828be9c467073f5d4239d7465
jantgg <smosh360@hotmail.com>
2023-01-20 13:47:38 +0000

    first

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Favorite, Location, Charuser, Episode, Epiuser, Locuser
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,56 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/users', methods=['GET'])
+def get_all_Users():
+    users = User.query.all()
+    users_serialized = [x.serialize() for x in users]
+    return jsonify({"body": users_serialized}), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+@app.route('/characters', methods=['GET'])
+def get_all_characters():
+    characters = Character.query.all()
+    characters_serialized = [x.serialize() for x in characters]
+    return jsonify({"body": characters_serialized}), 200
+
+@app.route('/favorites', methods=['GET'])
+def get_all_favorites():
+    favorites = Favorite.query.all()
+    favorites_serialized = [x.serialize() for x in favorites]
+    return jsonify({"body": favorites_serialized}), 200
+
+@app.route('/episodes', methods=['GET'])
+def get_all_episodes():
+    episodes = Episode.query.all()
+    episodes_serialized = [x.serialize() for x in episodes]
+    return jsonify({"body": episodes_serialized}), 200
+
+@app.route('/locations', methods=['GET'])
+def get_all_locations():
+    locations = Location.query.all()
+    locations_serialized = [x.serialize() for x in locations]
+    return jsonify({"body": locations_serialized}), 200
+
+@app.route('/charusers', methods=['GET'])
+def get_all_charusers():
+    charusers = Charuser.query.all()
+    charusers_serialized = [x.serialize() for x in charusers]
+    return jsonify({"body": charusers_serialized}), 200
+
+@app.route('/epiusers', methods=['GET'])
+def get_all_epiusers():
+    epiusers = Epiuser.query.all()
+    epiusers_serialized = [x.serialize() for x in epiusers]
+    return jsonify({"body": epiusers_serialized}), 200
+
+@app.route('/locusers', methods=['GET'])
+def get_all_locusers():
+    locusers = Loc_user.query.all()
+    locusers_serialized = [x.serialize() for x in locusers]
+    return jsonify({"body": locusers_serialized}), 200
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,17 +3,129 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+    user_name = db.Column(db.String(250), nullable=False, unique=True)
+    first_name = db.Column(db.String(250), nullable=False)
+    last_name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False, unique=True)
+    active = db.Column(db.Boolean, default=True)
 
     def serialize(self):
         return {
             "id": self.id,
+            "user_name": self.user_name,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "active": self.active,
         }
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    char_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    epi_id = db.Column(db.Integer, db.ForeignKey('episode.id'))
+    loc_id = db.Column(db.Integer, db.ForeignKey('location.id'))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+        }
+
+class Character(db.Model):
+    __tablename__ = 'character'
+    id = db.Column(db.Integer, primary_key=True)
+    char_named = db.Column(db.String(250), nullable=False, unique=True)
+    char_img = db.Column(db.String(250), nullable=False, unique=True)
+    char_content = db.Column(db.String(250), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "char_named": self.char_named,
+            "char_img": self.char_img,
+            "char_content": self.char_content,
+        }
+
+class Charuser(db.Model):
+    __tablename__ = 'charuser'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship(User)
+    char_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    char = db.relationship(Character)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "char_id": self.char_id,
+        }
+
+class Episode(db.Model):
+    __tablename__ = 'episode'
+    id = db.Column(db.Integer, primary_key=True)
+    epi_name = db.Column(db.String(250), nullable=False, unique=True)
+    epi_img = db.Column(db.String(250), nullable=False, unique=True)
+    epi_content = db.Column(db.String(250), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "char_name": self.char_name,
+            "char_img": self.char_img,
+            "char_content": self.char_content,
+        }
+
+
+class Epiuser(db.Model):
+    __tablename__ = 'epiuser'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship(User)
+    epi_id = db.Column(db.Integer, db.ForeignKey('episode.id'))
+    epi = db.relationship(Episode)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "epi_id": self.epi_id,
+        }
+
+
+class Location(db.Model):
+    __tablename__ = 'location'
+    id = db.Column(db.Integer, primary_key=True)
+    loc_name = db.Column(db.String(250), nullable=False, unique=True)
+    loc_img = db.Column(db.String(250), nullable=False, unique=True)
+    loc_content = db.Column(db.String(250), nullable=False)
+    def serialize(self):
+        return {
+            "id": self.id,
+            "loc_name": self.loc_name,
+            "loc_img": self.loc_img,
+            "loc_content": self.loc_content,
+        }
+
+
+
+class Locuser(db.Model):
+    __tablename__ = 'locuser'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship(User)
+    loc_id = db.Column(db.Integer, db.ForeignKey('location.id'))
+    loc = db.relationship(Location)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "loc_id": self.loc_id,
+        }
+
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
